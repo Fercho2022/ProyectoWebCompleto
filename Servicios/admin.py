375b2a0e2dfcf0caae441e1edcd7e9de3caaa402
fernando <crosio.fernando@gmail.com>
2022-07-13 22:09:06 -0300

    Se registra el models Servicio en el panel de administración

--- Servicios/admin.py
+++ Servicios/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from Servicios.models import Servicio
 
 # Register your models here.
+
+
+class ServicioAdmin(admin.ModelAdmin):
+
+    readonly_fields=('created', 'updated')    # con esta instrucción de solo lectura hago que los campos created y updated se reflejen en el panel de administración
+
+
+admin.site.register(Servicio, ServicioAdmin) # con esta instruccón se registra el modelo en el panel de administración
